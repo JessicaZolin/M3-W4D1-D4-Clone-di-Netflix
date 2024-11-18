aadbfaad8702036f10067c11e12b90b9e2db7b0e
Jessica Zolin <158849129+JessicaZolin@users.noreply.github.com>
2024-11-18 14:56:58 +0100

    modifica HTML con caricamento tramite JS anche dei due button prev e next

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 /* Funzione per popolare una riga di film */
 
 function populateMovieRow(rowId, start, end) {
-    const row = document.getElementById(rowId);
-    for (let i = start; i <= end; i++) {
-        const imagePath = `assets/imgs/movies/${i}.png`;
-        const card = createMovieCard(imagePath, i, i === start);
+    const row = document.getElementById(rowId);                     // richiama il div dove inserire le foto del carosello
+    const buttonPrev = document.createElement("button")             // crea il primo bottone: prev
+    buttonPrev.className = "nav-button prev"
+    buttonPrev.setAttribute("data-bs-target", "#carousel-1")
+    buttonPrev.setAttribute("type", "button")
+    buttonPrev.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" /></svg>'
+    row.appendChild(buttonPrev)
+    const buttonNext = document.createElement("button")             // crea il secondo bottone: next
+    buttonNext.className = "nav-button next"
+    buttonNext.setAttribute("data-bs-target", "#carousel-1")
+    buttonNext.setAttribute("type", "button")
+    buttonNext.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" /></svg>'
+    row.appendChild(buttonNext)
+    for (let i = start; i <= end; i++) {                           // cicla e crea le card per ogni carosello 
+        const imagePath = `assets/imgs/movies/${i}.png`;           // prende l'immagine dalla cartella
+        const card = createMovieCard(imagePath, i, i === start);   // crea la card avviando la funzione sottostante
         row.appendChild(card);
     }
 }
 
+
+
 /* Funzione per creare una card del film */
 function createMovieCard(imagePath, i, isFirst) {
-    const card = document.createElement('div');
+    const card = document.createElement('div');                 // crea un div
     card.className = "item d-flex";
-    if (isFirst) {                                              // Aggiungi la classe ms-2 al primo elemento
-        card.classList.add("ms-2"); 
+    if (isFirst) {                                              // Aggiungi la classe ms-2 solo al primo elemento
+        card.classList.add("ms-2");
     }
     card.innerHTML = `<img class="rounded-3" id="img${i}" type="button" data-bs-toggle="modal" data-bs-target="#film${i}" src="${imagePath}" alt="Movie Thumbnail-${i}">`;
     return card;
@@ -24,8 +38,14 @@ function createMovieCard(imagePath, i, isFirst) {
 document.addEventListener("DOMContentLoaded", () => {
     populateMovieRow("carousel-1", 1, 6);
     populateMovieRow("carousel-2", 7, 12);
-    populateMovieRow("carousel-3", 13, 18); 
-}); 
+    populateMovieRow("carousel-3", 13, 18);
+});
+
+
+
+
+/* Funzione per popolare le schede descrittive dei film */
+
 
 
 
